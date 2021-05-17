8032d6dda6d881a8c206ad8bcf2dfe94ae23c34e
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-05-18 09:08:25 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SDV503Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello John");
+            Console.WriteLine("It is i your friend");
+            Console.ReadKey();
+        }
+    }
+}
